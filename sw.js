8377ea7966dcc801c5331a88a18670a6e7249127
ethanhuang0405-bit <ethanhuang0405@gmail.com>
2026-08-28 17:51:22 +0800

    Add sw.js for trip workspace

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='turkiye-trip-v1',ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))))});
+
